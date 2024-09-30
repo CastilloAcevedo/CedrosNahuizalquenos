2f078cb2f7ba028cd6ea7a626b20038b009df194
EstefanyFuentes <fo21001@ues.edu.sv>
2024-09-29 21:15:42 -0600

    :memo: Se mejoro la eficiencia de las imagenes y el js

--- wwwroot/js/Admin.js
+++ wwwroot/js/Admin.js
@@ -2,7 +2,7 @@
 
 function updateCartCount(cartCount) {
     const cartItemCount = document.getElementById('cartItemCount');
-
+    cartCount++;
     if (cartCount > 0) {
         cartItemCount.textContent = cartCount;
         cartItemCount.style.display = 'inline-block'; // Mostrar el círculo rojo
@@ -11,6 +11,3 @@ function updateCartCount(cartCount) {
     }
 }
 
-// Simulación de la cantidad de productos en el carrito
-let cartCount = 3; // Aquí puedes obtener la cantidad desde tu servidor o localStorage
-updateCartCount(cartCount);
